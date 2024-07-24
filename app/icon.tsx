effa9487cd22e2e3d6f6b1834780de95d95a64ad
SeniorIntern <tnikhil.official@gmail.com>
2024-07-24 14:31:40 +0545

    Fix typos

--- app/icon.tsx
+++ app/icon.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { ImageResponse } from 'next/og';
 
-// Route segment config
-export const runtime = 'edge';
-
 // Image metadata
 export const size = {
   width: 32,
